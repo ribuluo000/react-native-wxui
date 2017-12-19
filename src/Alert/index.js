d943ae51e9a7f241e4d24958f9e2ac0d812e6934
ribuluo000 <ribuluo000@163.com>
2017-12-19 11:43:31 +0800

    import PropTypes from 'prop-types';
    import PropTypes from 'prop-types';

--- src/Alert/index.js
+++ src/Alert/index.js
@@ -1,5 +1,5 @@
 'use strict';
-import React, { Component, PropTypes } from 'react';
+import React, { Component, } from 'react';
 import {
     View,
     Text,
@@ -11,6 +11,8 @@ import {
 import RootSiblings from 'react-native-root-siblings';
 import * as Animatable from 'react-native-animatable';
 import * as Util from '../Util';
+import PropTypes from 'prop-types';
+
 export function showAlert(options, onConfirm, onCancel) {
     Alert.showAlert(options, onConfirm, onCancel);
 }
